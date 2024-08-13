5c6c0451dc9f25a80294779f51f1b21aa7c4d8e2
Devangam <92856944+Devangam@users.noreply.github.com>
2024-08-13 17:21:05 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,9 +5,9 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-udo apt install maven -y
+sudo apt install maven -y
 git clone https://github.com/Devangam/jpetstore-6.git
-cd /jpetstore
+cd jpetstore-6
 mvn package
-docker build -t petstore .
+docker build -t devangam/petstore .
 docker run -d -p 8080:8080 devangam/petstore
