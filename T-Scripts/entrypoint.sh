d995e4837f65e5567581ccaae20c7ae3b089e5f6
Devangam <92856944+Devangam@users.noreply.github.com>
2024-08-13 17:53:42 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y docker.io
-sudo chmod 666 /var/run/docker.sock
-sudo systemctl start docker
-sudo usermod -aG docker ubuntu
-sudo systemctl restart docker
-docker run -d -p 8081:8080 devangam/nginx
+sudo apt update -y | tee /tmp/apt-update.log
+sudo apt install -y docker.io | tee /tmp/apt-install-docker.log
+sudo chmod 666 /var/run/docker.sock | tee /tmp/docker-sock-permissions.log
+sudo systemctl start docker | tee /tmp/docker-start.log
+sudo usermod -aG docker ubuntu | tee /tmp/docker-usermod.log
+sudo systemctl restart docker | tee /tmp/docker-restart.log
+sleep 10
+docker run -d -p 8081:8080 devangam/nginx | tee /tmp/docker-run.log
