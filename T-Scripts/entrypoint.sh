706f8944da1d0d56260d98991cb4844224b0cf6e
Devangam <92856944+Devangam@users.noreply.github.com>
2024-08-13 17:33:02 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,9 +5,4 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-sudo apt install maven -y
-git clone https://github.com/Devangam/secretsanta-generator.git
-cd secretsanta-generator
-mvn package
-docker build -t devangam/secretsanta-generator .
-docker run -d -p 8080:8080 devangam/secretsanta-generator
+docker run -d -p 8080:8080 adijaiswal/petsore
